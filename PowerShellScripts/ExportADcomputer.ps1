6dbf059bbb7c1149a1ee93fbb1be245ed5dd001a
José Abelardo Vicente Filho <maxabelardo@hotmail.com>
2022-03-14 17:48:17 -0300

    a

--- PowerShellScripts/ExportADcomputer.ps1
+++ PowerShellScripts/ExportADcomputer.ps1
@@ -1,21 +1,26 @@
 ﻿#
-# Este script foi criado para extrair os usuários e suas informações do Active Directory e inserir elas 
+# Este script foi criado para extrair os "Computadores" e suas informações do Active Directory e inserir elas 
 # em um servido de banco dados para futuro tratamento.
 # O script foi criado para ser executado de dentro de um JOB do agent do SQL Server.
 
 #Variáveis do servido e banco de dados
-$SQLInstance = "XXXXXXXX"
-$SQLDatabase = "DBActiveDirectory"
+$SQLInstance = "XXXXXXXX" # Nome da estância de banco de dados
+$SQLDatabase = "DBActiveDirectory" # Nome da base de dados
 
 #Parametro necessário para execução do script dentro do job
 Set-Location C:
 
-
+#Script:
 # Limpeza da tabela de STAGE que reseberá os dados brutos
  $SQLQueryDelete = "USE $SQLDatabase
     TRUNCATE TABLE [AD].[STGADComputer]"
-
+    
+#Executando o script carragado na variável "$SQLQueryDelete"
 $SQLQuery1Output = Invoke-Sqlcmd -query $SQLQueryDelete -ServerInstance $SQLInstance
+    #Variável que recebe a saida da execução: $SQLQuery1Output
+    #comando que executa o script: "Invoke-Sqlcmd"
+    #Paramentro que indica que será executado um script dentro de uma variável "-query"
+    #Paramentro que define a extancia de banco de dados que será executado script "-ServerInstance"
 
 
 #======= ATENÇÃO ========#
@@ -26,32 +31,30 @@ $Iniciais = 'a*','b*','c*','d*','e*','g*','h*',
 'i*','j*','k*','l*','m*','n*','o*','p*','q*','r*','s*','t*','u*','x*','z*','w*',
 '1*','2*','3*','4*','5*','6*','7*','8*','9*','0*'
 
-
-
 #Loop das iniciais
 ForEach($Inicial in $Iniciais){
 
-
 #Iniciar a extração dos Usuários do Active Directory
 # A variável "$Usrs" é uma matriz que receberá o resultado do comando de extração dos usuários.
+    try{
+    $Usrs = Get-ADComputer -f {Name -like $Inicial} -Properties * | SELECT SID, Name, DisplayName, SamAccountName, Description, ObjectClass, PrimaryGroup, MemberOf,
+        OperatingSystem, OperatingSystemHotfix, OperatingSystemServicePack, OperatingSystemVersion,
+        CanonicalName, Enabled,IPv4Address, 
+        @{Name='Created';Expression={$_.Created.ToString("yyyy\/MM\/dd HH:mm:ss")}},
+        @{Name='Deleted';Expression={$_.Deleted.ToString("yyyy\/MM\/dd HH:mm:ss")}},
+        @{Name='Modified';Expression={$_.Modified.ToString("yyyy\/MM\/dd HH:mm:ss")}},
+        @{Name='LastLogonDate';Expression={$_.LastLogonDate.ToString("yyyy\/MM\/dd HH:mm:ss")}},
+        logonCount,
+        PasswordExpired, 
+        @{Name='PasswordLastSet';Expression={$_.PasswordLastSet.ToString("yyyy\/MM\/dd HH:mm:ss")}}, 
+        AuthenticationPolicy -ErrorAction stop
+    }catch{
+        Write-Output $Inicial
+    throw $_
+    break
+    }
 
-try{
- $Usrs = Get-ADComputer -f {Name -like $Inicial} -Properties * | SELECT SID, Name, DisplayName, SamAccountName, Description, ObjectClass, PrimaryGroup, MemberOf,
-    OperatingSystem, OperatingSystemHotfix, OperatingSystemServicePack, OperatingSystemVersion,
-    CanonicalName, Enabled,IPv4Address, 
-    @{Name='Created';Expression={$_.Created.ToString("yyyy\/MM\/dd HH:mm:ss")}},
-    @{Name='Deleted';Expression={$_.Deleted.ToString("yyyy\/MM\/dd HH:mm:ss")}},
-    @{Name='Modified';Expression={$_.Modified.ToString("yyyy\/MM\/dd HH:mm:ss")}},
-    @{Name='LastLogonDate';Expression={$_.LastLogonDate.ToString("yyyy\/MM\/dd HH:mm:ss")}},
-    logonCount,
-    PasswordExpired, 
-    @{Name='PasswordLastSet';Expression={$_.PasswordLastSet.ToString("yyyy\/MM\/dd HH:mm:ss")}}, 
-    AuthenticationPolicy -ErrorAction stop
-}catch{
-Write-Output $Inicial
-throw $_
-break
-}
+
 
 
 #Loop que será usuado para transferir os dados da matriz para o banco de dados
